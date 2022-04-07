9aaf371c89b7dad920946a0ef3d237e1fc5e5497
WelcometoGitHubUz <necrmetrology@gmail.com>
2022-04-07 16:48:49 +0500

    7 april

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, example,doughnot,linechart
 urlpatterns = [
     path('', home),
+    path('example', example),
+    path('doughnot', doughnot),
+    path('linechart',linechart)
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'index.html', context)
+
+def example(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+
+    return render(request, 'example.html', context)
+
+def doughnot(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+
+    return render(request, 'details/doughnot.html',context)
+def linechart(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+
+    return render(request, 'details/linechart.html',context)
